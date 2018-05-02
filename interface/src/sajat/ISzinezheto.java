572bc94995b5dc93ab5e731ec985462a17f46e41
bogar2 <36476441+bogar2@users.noreply.github.com>
2018-05-02 13:01:31 +0200

    Add files via upload
    interface

---
+++ interface/src/sajat/ISzinezheto.java
@@ -0,0 +1,11 @@
+package sajat;
+
+import java.awt.Color;
+
+public interface ISzinezheto {
+	Color alapertelmezett = Color.red;
+	
+	Color getAlapertelmezett();
+	void setAlapertelmezett(Color szin);
+
+}
